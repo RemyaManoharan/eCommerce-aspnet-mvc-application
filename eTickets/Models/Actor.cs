8c916837d4dd2b46654b761ccb40d4ca06220fe4
RemyaManoharan <remyajinil@outlook.com>
2023-11-12 17:07:05 +0100

    added model classes

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+    {
+    public class Actor
+    {
+        [Key]
+        public int ID { get; set; }
+        public string ProfilePictureURL {  get; set; }
+        public string FullNAme { get; set;}
+        public string Bio { get; set;  }
+         
+    }
+}
